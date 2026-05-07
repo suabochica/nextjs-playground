d62a078a6f3c316d9e023e103083f505df37af27
Sergio L. Benítez D. <sergio.kun21@gmail.com>
2026-05-07 11:00:10 -0500

    myspace: protect routes.

--- packages/03-myspace/app/page.tsx
+++ packages/03-myspace/app/page.tsx
@@ -1,11 +1,22 @@
-export default function Home() {
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+
+export default async function Home() {
+  const session = await getServerSession();
+
+  if (!session) {
+    redirect("/api/auth/signin");
+  }
+
   return (
-    <div>
-      <h1>Welcome to NextSpace!</h1>
-      <p>
-        A next-gen social media app to connect with frens inspired by MySpace
-      </p>
-      <p>To get started, sign up for an account</p>
-    </div>
+    <main>
+      <div>
+        <h1>Welcome to NextSpace!</h1>
+        <p>
+          A next-gen social media app to connect with frens inspired by MySpace
+        </p>
+        <p>To get started, sign up for an account</p>
+      </div>
+    </main>
   );
 }
